969fe89a4dd4d25a3dd1f01bc8df00485b3cc17a
unknown <ebteshamhossain98@gmail.com>
2021-08-26 19:55:04 +0600

    second

--- app.js
+++ app.js
@@ -1,4 +1,23 @@
 // Json
 
+
 const user = { id: 11, name: 'gorib amir', job: 'actor' };
-console.log(user);
+const stringified = JSON.stringify(user);
+console.log(user)
+console.log(stringified);
+
+const shop = {
+    name: 'alia store',
+    address: 'ranbir road',
+    products: ['laptop', 'mobile', 'pepsi'],
+    owner {
+        name: 'alia';
+        profession: 'navel dancer';
+
+    }
+    isExpensive: false
+}
+const shopStringified = JSON.stringify(shop);
+const converted = JSON.parse(shopStringified);
+console.log(shopStringified);
+// console.log(shop);
